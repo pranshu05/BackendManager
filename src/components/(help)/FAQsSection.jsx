933a292341ab3fe55e0b3b4bb574474d9c83c7a2
Parv <202301157@dau.ac.in>
2025-12-01 12:52:07 +0530

    Changed video

--- src/components/(help)/FAQsSection.jsx
+++ src/components/(help)/FAQsSection.jsx
@@ -128,7 +128,7 @@ const FAQsSection = () => {
                         <iframe
                             width="100%"
                             height="100%"
-                            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                            src="https://www.youtube.com/embed/ZMuJvdaLl1s"
                             title="DBuddy Tutorial"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
